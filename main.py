fe077f5872c29d4208c1eff4430f4b71015dcd18
Choudhuri <choudhru@oregonstate.edu>
2023-05-17 10:53:28 -0700

    Task 3 not working

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-from selenium.webdriver.chrome.webdriver import WebDriver
-from noaa_sdk import NOAA # NOAA-SDK python
+from selenium import webdriver
+from noaa_sdk import NOAA
 
 # Global Variables
 n = NOAA()
@@ -9,76 +8,68 @@ pallin_num = 112211
 non_pallin_num = 12345
 
 # Test Functions
-# def check_dir(path):
-    # # Create directory to save the file
-    # path: str = os.getcwd()
-    # parent_path: str = os.path.abspath(os.path.join(path, os.pardir))
-    # out_dir_path: str = os.path.join(parent_path, "HTML_output")
-    # os.makedirs(out_dir_path)
-
-def check_pallindrome(num):
+def check_palindrome(num):
     num = str(num)
-    num_copy = num
-    num = num[::-1]
-    if num_copy != num:
-        return True
-    return False
+    return num == num[::-1]
 
-def check_webpage(link: str) -> None:
-    # Initialize Chrome WebDriver
-    driver: WebDriver = WebDriver()
-    # Navigate to webpage
-    driver.get(link)
-    
-    # Wait for page to load and all elements to become visible
-    driver.implicitly_wait(10)
-    # Get all visible elements on the page
-    visible_elements = driver.find_elements_by_css_selector("*:not([style*=‘display:none’]):not([style*=‘display: none’])")
-    # Save visible elements to an HTML file
-    with open("visible_elements.html", "w", encoding="utf-8") as file:
-        file.write("<html><body>")
-        for element in visible_elements:
-            file.write(element.get_attribute("outerHTML"))
-        file.write("</body></html>")
-    # Close the WebDriver
-    driver.quit()
-
-
-# Using the NOAA-SDK in python. Use the lat and lon to get forecast for a specific location.
-def check_weather():
-    lat = 40.7314
-    lon = -73.8656
+def check_weather(postal_code, country):
     try:
-        forecasts = n.get_forecasts(coordinates=(lat, lon))
-        print("Test 2 passed")
-    except Exception as err:
-        print("Test 2 failed")
-        print(err)
-
-
-
-def main():
-    try: # Check the pallindrome function
-        if check_pallindrome(pallin_num) == False or check_pallindrome(non_pallin_num) == True:
-            raise Exception
-        else:
-            print("Test 1 passed.")
-    except Exception as err:
-        print("Test case 1 failed. Please correct the pallindrome function")
-        print(err)
-
-    try: # Check the weather function
-        check_weather()
+        forecasts = n.get_forecasts(postal_code, country)
+        print("Test case 2 passed.")
+        # Process forecast data as needed
     except Exception as err:
         print("Test case 2 failed.")
         print(err)
 
-    try: # Check the webpage function
-        check_webpage(link)
+def check_webpage(link):
+    # Initialize Chrome WebDriver
+    driver = webdriver.Chrome()
+    try:
+        # Navigate to webpage
+        driver.get(link)
+        # Wait for page to load and all elements to become visible
+        driver.implicitly_wait(10)
+        # Get all visible elements on the page
+        visible_elements = driver.find_elements_by_css_selector(
+            "*:not([style*='display:none']):not([style*='display: none'])"
+        )
+        # Save visible elements to an HTML file
+        with open("visible_elements.html", "w", encoding="utf-8") as file:
+            file.write("<html><body>")
+            for element in visible_elements:
+                file.write(element.get_attribute("outerHTML"))
+            file.write("</body></html>")
         print("Test case 3 passed.")
     except Exception as err:
         print("Test case 3 failed.")
         print(err)
+    finally:
+        # Close the WebDriver
+        driver.quit()
 
-if __name__=="__main__":
+def main():
+    try:  # Check the palindrome function
+        if not check_palindrome(pallin_num) or check_palindrome(non_pallin_num):
+            raise Exception("Palindrome test failed.")
+        else:
+            print("Test case 1 passed.")
+    except Exception as err:
+        print("Test case 1 failed.")
+        print(err)
+
+    try:  # Check the weather function
+        postal_code = "97330"
+        country = "US"
+        check_weather(postal_code, country)
+    except Exception as err:
+        print("Test case 2 failed.")
+        print(err)
+
+    try:  # Check the webpage function
+        check_webpage(link)
+    except Exception as err:
+        print("Test case 3 failed.")
+        print(err)
+
+if __name__ == "__main__":
     main()
