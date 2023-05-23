9daf38f5f55505248c87e97cf86e4bef2221431b
Choudhuri <choudhru@oregonstate.edu>
2023-05-23 14:37:58 -0700

    task 3

--- main.py
+++ main.py
@@ -17,27 +17,24 @@ non_pallin_num = 12345
 
 def check_pallindrome(num):
     num = str(num)
-    num_copy = num
-    num = num[::-1]
-    if num_copy != num:
-        return True
-    return False
+    return num == num[::-1]
 
 def check_webpage(link: str) -> None:
     # Initialize Chrome WebDriver
-    driver: WebDriver = WebDriver()
+    driver = WebDriver()
     # Navigate to webpage
     driver.get(link)
 
     # Wait for page to load and all elements to become visible
     driver.implicitly_wait(10)
     # Get all visible elements on the page
-    visible_elements = driver.find_elements_by_css_selector("*:not([style*=‘display:none’]):not([style*=‘display: none’])")
+    visible_elements = driver.find_element,("css selector", "*:not([style*=‘display:none’]):not([style*=‘display: none’])")
     # Save visible elements to an HTML file
     with open("visible_elements.html", "w", encoding="utf-8") as file:
         file.write("<html><body>")
         for element in visible_elements:
-            file.write(element.get_attribute("outerHTML"))
+            source_code = element.get_attribute('outerHTML')
+            file.write()
         file.write("</body></html>")
     # Close the WebDriver
     driver.quit()
@@ -47,12 +44,8 @@ def check_webpage(link: str) -> None:
 def check_weather():
     lat = 40.7314
     lon = -73.8656
-    try:
-        forecasts = n.get_forecasts(coordinates=(lat, lon))
-        print("Test 2 passed")
-    except Exception as err:
-        print("Test 2 failed")
-        print(err)
+    forecasts = n.points_forecast(40.7314, -73.8656, type='forecastGridData')
+
 
 
 
@@ -61,13 +54,14 @@ def main():
         if check_pallindrome(pallin_num) == False or check_pallindrome(non_pallin_num) == True:
             raise Exception
         else:
-            print("Test 1 passed.")
+            print("Test case 1 passed.")
     except Exception as err:
         print("Test case 1 failed. Please correct the pallindrome function")
         print(err)
 
     try: # Check the weather function
         check_weather()
+        print("Test case 2 passed.")
     except Exception as err:
         print("Test case 2 failed.")
         print(err)
